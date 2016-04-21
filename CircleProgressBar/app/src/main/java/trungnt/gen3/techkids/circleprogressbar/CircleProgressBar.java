57acb0c8604439db1a2643d7ab13f614ece94363
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-21 10:13:22 +0700

    draw angle from 0 to 45 of Circle (temp)

--- CircleProgressBar/app/src/main/java/trungnt/gen3/techkids/circleprogressbar/CircleProgressBar.java
+++ CircleProgressBar/app/src/main/java/trungnt/gen3/techkids/circleprogressbar/CircleProgressBar.java
@@ -25,8 +25,9 @@ public class CircleProgressBar extends View {
         paint.setColor(Color.GREEN);
         paint.setStyle(Paint.Style.FILL);
 
-        //ve o vi tri nao: x, y, width, height cua rectF
-        RectF rectF = new RectF(0, 100, 50, 200); //don vi ve trong java la pizel, con don vi tren xml la dp
-        canvas.drawRect(rectF, paint);
+        //ve o vi tri nao: x, y, width, height cua rectF => left, top, right, bottom
+        RectF rectF = new RectF(0, 0, 200, 200); //don vi ve trong java la pizel, con don vi tren xml la dp
+        //rectF la vung Cha bao quanh hinh tron, goc quay tu 0 den 45 do la goc quay, ve o giua tam cua hinh chu nhat, paint  la but ve
+        canvas.drawArc(rectF, 0, 45, true, paint);
     }
 }
