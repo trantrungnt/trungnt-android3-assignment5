813f03f0bd6096d694748c3c588f35e2346df1b0
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-22 02:50:34 +0700

    use drawCircle method to draw a Red Circle (temp)

--- CircleProgressBar/app/src/main/java/trungnt/gen3/techkids/circleprogressbar/CircleProgressBar.java
+++ CircleProgressBar/app/src/main/java/trungnt/gen3/techkids/circleprogressbar/CircleProgressBar.java
@@ -22,14 +22,22 @@ public class CircleProgressBar extends View {
     public void draw(Canvas canvas)
     {
         super.draw(canvas);
-        Paint paint = new Paint();
-        paint.setColor(Color.GREEN);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(10);
+//        Paint paint = new Paint();
+//        paint.setColor(Color.GREEN);
+//        paint.setStyle(Paint.Style.STROKE);
+//        paint.setStrokeWidth(10);
+//
+//        //ve o vi tri nao: x, y, width, height cua rectF => left, top, right, bottom
+//        RectF rectF = new RectF(100, 100, 500, 500); //don vi ve trong java la pizel, con don vi tren xml la dp
+//        //rectF la vung Cha bao quanh hinh tron, goc quay tu 0 den 45 do la goc quay, ve o giua tam cua hinh chu nhat, paint  la but ve
+//        canvas.drawArc(rectF, 0, 45, false, paint);
 
-        //ve o vi tri nao: x, y, width, height cua rectF => left, top, right, bottom
-        RectF rectF = new RectF(100, 100, 500, 500); //don vi ve trong java la pizel, con don vi tren xml la dp
-        //rectF la vung Cha bao quanh hinh tron, goc quay tu 0 den 45 do la goc quay, ve o giua tam cua hinh chu nhat, paint  la but ve
-        canvas.drawArc(rectF, 0, 45, false, paint);
+        Paint p = new Paint();
+        // smooths
+        p.setAntiAlias(true);
+        p.setColor(Color.RED);
+        p.setStyle(Paint.Style.STROKE);
+        p.setStrokeWidth(4.5f);
+        canvas.drawCircle(300, 280, 200, p);
     }
 }
