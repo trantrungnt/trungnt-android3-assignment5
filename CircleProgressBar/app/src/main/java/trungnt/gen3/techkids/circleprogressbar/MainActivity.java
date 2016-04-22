c118af0face70848fb1da0293f7d2c287b324598
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-22 16:15:37 +0700

    use imgBtnProcess to run Timer

--- CircleProgressBar/app/src/main/java/trungnt/gen3/techkids/circleprogressbar/MainActivity.java
+++ CircleProgressBar/app/src/main/java/trungnt/gen3/techkids/circleprogressbar/MainActivity.java
@@ -3,13 +3,16 @@ package trungnt.gen3.techkids.circleprogressbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private int countTime;
     private CircleProgressBar circleProgressBar;
+    private ImageButton imgBtnProcess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +26,25 @@ public class MainActivity extends AppCompatActivity {
         circleProgressBar = (CircleProgressBar) this.findViewById(R.id.circleProgressBar);
         circleProgressBar.setMax(20);
         circleProgressBar.setProgress(0);
+        imgBtnProcess = (ImageButton) this.findViewById(R.id.imgBtnProcess);
+        imgBtnProcess.setOnClickListener(this);
+    }
 
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                countTime++;
-                MainActivity.this.circleProgressBar.setProgress(countTime);
-                MainActivity.this.circleProgressBar.postInvalidate(); //giong repaint
-            }
-        }, 1000, 1000);
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if (id == R.id.imgBtnProcess) {
+            Timer timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    countTime++;
+                    MainActivity.this.circleProgressBar.setProgress(countTime);
+                    MainActivity.this.circleProgressBar.postInvalidate(); //giong repaint
+                }
+            }, 1000, 1000);
+
+        }
     }
 }
